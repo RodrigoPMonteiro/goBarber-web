56ec40c233c9d32c7bb1ab8ce855c9fbb0544b8a
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-07-12 22:52:22 -0300

    Class finished: Login validation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 // como ainda nao tem rotas, importa a Pagina SignIn direto no App.tsx
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
