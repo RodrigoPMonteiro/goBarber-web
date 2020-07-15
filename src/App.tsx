81e3e0407963a59af8aa8b9e93d14dd0e6caa2a9
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-07-14 23:28:33 -0300

    Class finished: Application Logout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GlobalStyle from './styles/global';
 // como ainda nao tem rotas, importa a Pagina SignIn direto no App.tsx
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
